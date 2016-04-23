c6871358eac6c2c2602cd51fa6609da4f7ca405d
Felipe Oliveira Colen <felipecolen@gmail.com>
2016-04-23 15:24:21 -0400

    Ajustando o readme e melhorando o codigo do Vota

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# aulaspoo2016-1
-Exercícios feitos nas aulas de POO turma 2016-1
+# aulaspoo2016-1 IFRO PVH Zona Norte
+Exercícios feitos nas aulas de POO turma 2016-1 - Informática para Internet
+
+
+## Helps
+Você pode contribuir através de um fork desse repo, melhorando/otimizando os códigos. A ideia inicial é aprender um pouco sobre a linguagem Java e assim conseguir resolver os problemas das maratonas de programação que estão disponíveis no site do SPOJ BR.
+
+
+## Informações úteis
+O nome do arquivo deve ser o mesmo nome da classe principal
+
+O comando "javac nome_do_arquivo.java" irá compilar o arquivo, que você poderá executar depois com o comando "java nome_do_arquivo" (que agora possui a extensão .class)
---
+++ Vota.java
@@ -0,0 +1,65 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Vota {
+
+    public static void main(String[] args) {
+    	// obtem a data e hora atuais
+        LocalDate data = LocalDate.now();
+                        
+        System.out.println("Digite a sua data de Nascimento");
+        System.out.println("no formato 01/01/1991");
+        
+
+        Scanner entrada = new Scanner(System.in);
+        // a variavel dtnasc recebe o conteudo digitado        
+        String dtnasc = entrada.nextLine();
+        
+        // agora, a var do tipo vetor dtsplit, recebe a dtnasc digitada e dividida em 3 partes, 
+        // o método split ira quebrar a string sempre que houver o /
+        String[] dtsplit = dtnasc.split("/");
+
+        // para calcular corretamente, precisamos verificar se temos realmente uma data no padrão 01/01/1991
+        // se sim, então temos um vetor com 3 casas
+        if (dtsplit.length == 3) {        
+	        // agora, usando a mesma variavel que recebeu a data digitada
+	        // alteramos a ordem do conteúdo para o padrão americano "ano-mês-dia"
+	        dtnasc = dtsplit[2] +"-"+ dtsplit[1] +"-"+ dtsplit[0];		
+	        
+	        // para que possamos calcular a idade, por meio da Period, criamos uma variavel do tipo LocalDate
+	        // passando a data que foi digitada e ajustada 
+	        LocalDate datanasc = LocalDate.parse(dtnasc);
+
+	        // agora, com o método between (tradução "entre"), 
+	        // temos o período no intervalo entre a data de nascimento e a data atual
+	        Period idade = Period.between(datanasc, data);
+	        
+	        // exibimos a idade completa
+	        System.out.println("Sua idade é " + idade.getYears() + " anos, "
+	        		+ idade.getMonths() + " meses" 
+	        		+ " e " + idade.getDays() + " dias.");
+	        int anos = idade.getYears();
+	        
+	        if (anos < 0) {
+	        	System.out.println("Data de nascimento inválida.");
+	        }
+	        else if (anos >= 16 && anos < 18 || anos >= 65) {
+	            System.out.println("Voto facultativo!");
+	        }
+	        else if (anos >= 18 && anos < 65) {
+	            System.out.println("Voto obrigatório!");
+	        } 
+	        else {
+	            System.out.println("Desculpe, você não pode votar!");
+	        }
+	    }
+	    // se não, pedimos para digitar novamente
+	    else {
+	    	System.out.println("Favor digitar a data de nascimento no padrão 01/01/1991.");
+	    }
+    }
+}
