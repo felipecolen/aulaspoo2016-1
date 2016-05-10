c61f0f5465eaa4ed360c6131e8f30adec75eae33
Janneene <porpurina_irene@hotmail.com>
2016-05-09 21:03:59 -0400

    Update Vota.java
    :-)

--- Vota.java
+++ Vota.java
@@ -1,41 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package doprofessor;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
 
-/**
- *
+/*
  * @author Jann
+ADICIONADO:
+    - Pedido de nome;
+    - Contagem de regulares e irregulares
+    - Rodar mais de uma vez caso o usuário queira
+    - Comentários super explicativos; Heheh~ 
  */
 public class Vota {
     public static void main(String[] args) {
     	// obtem a data e hora atuais
         LocalDate data = LocalDate.now();
-                        
-        System.out.println("Digite a sua data de Nascimento");
-        System.out.println("no formato 01/01/1991");
-
-        Scanner entrada = new Scanner(System.in);
-        // a variavel dtnasc recebe o conteudo digitado        
-        String dtnasc = entrada.nextLine();
+        String fazer = "sim";   
+        int irr =0, reg=0; 
+        String verif = "", nome = "";
+        Scanner entrada = new Scanner(System.in);   
         
-        boolean verifica = VerificaClasseEleitoral(dtnasc, data);
-        if(verifica){
-            System.out.println("Você votou na última eleição?");
-        }  
-        else
-            System.out.println("Tudo bem, amiguinho?");
-        
+        //Verificar se a pessoa gostaria de fazer novamente após fazer
+        //pela primeira vez
+        while(fazer.equals("sim")){
+            //Nome do meliante
+            System.out.println("Entre com seu nome: ");
+            nome = entrada.nextLine();
+            //Idade da pessoa
+            System.out.println("\nDigite a sua data de Nascimento");
+            System.out.println("no formato 01/01/1991");
+            String dtnasc = entrada.nextLine();
+            //Faz uso da classe boleana verifica          
+            boolean verifica = VerificaClasseEleitoral(dtnasc, data);
+            //Caso o resultado de verifica for TRUE, vai executar o if abaixo
+            if(verifica){
+                System.out.println("\nVocê votou na última eleição, " + nome + 
+                        "? \n Digite sim ou nao");
+                verif = entrada.nextLine();
+                //Comparação de respostas
+                //caso for sim, irá incrementar a variável reg, que serve
+                //como contagem para os regulares
+                if(verif.equals("sim"))
+                    reg++;
+                //Tentei fazer verif.equals("não") só que mesmo respondendo não,
+                //ele não cai nesse else if
+                //caso a resposta for nao, irá incrementar a variável irr
+                //que é a contagem para os irregulares
+                else if(verif.equals("nao"))
+                    irr++;
+                //SE o usuário não digitar sim ou nao... irá cair no else
+                //que irá pedir para ele digitar conforme o pedido
+                else
+                    System.out.println("Digite conforme pedido");
+            }  
+            //Caso resposta da classe for FALSE, irá cair neste else
+            else
+                System.out.println("Tudo bem, amiguinho?");
+            
+            //Aqui a variável 'fazer' irá receber fim ou sim
+            //Caso receba fim, irá sair do laço do WHILE
+            System.out.println("\nGostaria de fazer de novo? \n Digite sim caso"
+                    + " queira \n Digite fim caso não queira\n");
+            fazer = entrada.nextLine();
+        }
+        //Finalizando o aplicativo, agradecemos o uso pelo mesmo e indicamos a
+        //quantidade de regulares e irregulares 
+        System.out.println("\n Agradecemos por usar nosso sistema! "
+                + "\n Quantidade de regulares = " + reg + 
+                " \n quantidade de irregulares = " + irr);
     }
     public static boolean VerificaClasseEleitoral(String dtnasc, LocalDate data){
-          // agora, a var do tipo vetor dtsplit, recebe a dtnasc digitada e dividida em 3 partes, 
+        // agora, a var do tipo vetor dtsplit, recebe a dtnasc digitada e dividida em 3 partes, 
         // o método split ira quebrar a string sempre que houver o /
+        
+        //o MÉTODO SPLIT irá quebrar a String DTNASC nas partes que envolver
+        //a barra ( / ), se o usuário digitar sua data corretamente
+        //o vetor DTSPLIT irá receber três valores em cada posição
+        //posição 0 recebe o dia : dtsplit[0]
+        //posição 1 recebe o mês : dtsplit[1]
+        //posição 2 recebe o ano : dtsplit[2]
         String[] dtsplit = dtnasc.split("/");
 
         // para calcular corretamente, precisamos verificar se temos realmente uma data no padrão 01/01/1991
@@ -59,7 +103,22 @@ public class Vota {
 	        		+ " e " + idade.getDays() + " dias.");
 	        int anos = idade.getYears();
 	        
-	        if (anos < 0) {
+                //Calculo para descobrir se o voto é obrigatorio, facultativo ou
+                //que não precisa votar
+                
+                /* AQUI É UMA PARTE MUITO IMPORTANTE, leia e releia para compreender
+                Como o nosso método retorna tipo BOOLEAN então só poderá retornar
+                TRUE ou FALSE. Note que nem todos os if, else if e else estão retornando
+                algo, simplesmente não há necessidade porque existe um RETURN FALSE
+                após todas as verificações.
+                CASO a variável 'anos' atenda uma das comparações que retornam TRUE
+                irá automaticamente sair DESTE MÉTODO, voltando para o principal 
+                e retornando TRUE;
+                CASO a variável 'anos' não atenda as comparações citadas, o método
+                irá continuar funcionando ATÉ achar um 'return', que no caso será
+                o RETURN FALSE do fim.
+                */
+	        if (anos < 0) { 
 	        	System.out.println("Data de nascimento inválida.");
 	        }
 	        else if (anos >= 16 && anos < 18 || anos >= 65) {
